7df8e3fde1bacb6f874cf1f89dcc683b08d62852
Suhadak Akbar <ggsuha@gmail.com>
2020-03-17 18:06:13 +0800

    create admin page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,10 +16,30 @@
 Route::get('/', function() {
     return redirect('/kdrama');
 });
-Route::get('/kdrama', 'InputController@kdramas');
+Route::get('/kdrama', 'InputController@kdramas')->name('front');
 Route::get('/kdrama/page/{page}', 'InputController@kdramas');
 Route::get('/kdrama/{slug}', 'InputController@kdrama');
 Route::get('/ost/{slug}', 'InputController@ost');
 Route::get('/ost/', 'InputController@osts');
 Route::get('/ost/page/{page}', 'InputController@osts');
 
+
+Auth::routes([
+  'register' => false,
+  'reset' => false,
+  'verify' => false,
+]);
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function(){
+    Route::get('/', function() {
+        return redirect('/admin/dasboard');
+    });
+    Route::get('/dasboard', 'HomeController@index');
+    Route::get('/artikel', 'ArticleController@index');
+    Route::get('/artikel/baru', 'ArticleController@create');
+    Route::post('/artikel/store', 'ArticleController@store')->name('store');
+
+    Route::post('/image/upload', 'CkEditorController@upload')->name('upload');
+
+    Route::get('/logout', 'HomeController@logout')->name('out');
+});
